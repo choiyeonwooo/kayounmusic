f3ee0f4bec23a6470a857951691d191ed1a3ce10
Brian Min <bymi15@yahoo.com>
2021-10-17 17:00:25 +0100

    Add responsive images for main page and refactor nav headings

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import logoImage from "../assets/images/logo.png";
 
 function Header() {
   return (
-    <header class="navigation">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
+    <header className="navigation">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
             <nav className="navbar navbar-expand-lg">
               <Container fluid>
                 <div className="navbar-header">
@@ -16,32 +16,28 @@ function Header() {
                 </div>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                  <ul class="nav navbar-nav ms-auto">
+                  <ul className="nav navbar-nav ms-auto">
                     <li>
                       <Nav.Link href="#home">Home</Nav.Link>
                     </li>
                     <li>
-                      <NavDropdown title="Portfolio">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                      <NavDropdown title="About">
+                        <NavDropdown.Item href="#action/3.1">Biography</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.4">Résumé</NavDropdown.Item>
                       </NavDropdown>
                     </li>
                     <li>
-                      <Nav.Link href="#link">Service</Nav.Link>
+                      <Nav.Link href="#link">Film Music</Nav.Link>
                     </li>
                     <li>
-                      <NavDropdown title="Blog">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                      </NavDropdown>
+                      <Nav.Link href="#link">Works</Nav.Link>
                     </li>
                     <li>
-                      <NavDropdown title="Pages">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                      <NavDropdown title="Scores">
+                        <NavDropdown.Item href="#action/3.1">
+                          Original Compositions
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.4">Arrangements</NavDropdown.Item>
                       </NavDropdown>
                     </li>
                     <li>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import "./assets/css/style.css";
 import Home from "./pages/Home";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <Home />
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <Home />
+    </React.StrictMode>,
+    rootElement
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
