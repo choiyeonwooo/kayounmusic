8747974a59b368a061bf25413d36e5ce22ccad86
Brian Min <bymi15@yahoo.com>
2021-10-25 00:20:52 +0100

    Add scores and works functions

--- db/models/score.go
+++ db/models/score.go
@@ -1,10 +1,13 @@
 package models
 
 type Score struct {
-	Id          string `bson:"_id" json:"id,omitempty"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	PreviewFile string `json:"previewFile"`
-	Category    string `json:"category"`
-	Date        bool   `json:"date"`
+	Id            string `bson:"_id" json:"id,omitempty"`
+	Title         string `json:"title"`
+	Description   string `json:"description"`
+	PreviewFile   string `json:"previewFile"`
+	Category      string `json:"category"`
+	Date          bool   `json:"date"`
+	SoundCloudUrl string `json:"soundCloudUrl"`
+	Img           string `json:"img"`
+	YoutubeId     string `json:"youtubeId"`
 }
--- functions/src/scores/scores.go
+++ functions/src/scores/scores.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/bymi15/kayounmusic/db"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	client := db.InitMongoClient()
+	scores, err := client.GetScores()
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 500,
+		}, err
+	}
+	jsonBody, _ := json.Marshal(scores)
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Headers:    map[string]string{"Content-Type": "text/plain"},
-		Body:       "Hello, World!",
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       string(jsonBody),
 	}, nil
 }
 
