d43c73eb7d379374bc135c56e354170e23082835
Brian Min <bymi15@yahoo.com>
2021-10-24 16:01:19 +0100

    Fix configs for functions

--- functions/filmmusics/filmmusics.go
+++ functions/filmmusics/filmmusics.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	return &events.APIGatewayProxyResponse{
-		StatusCode:        200,
-		Headers:           map[string]string{"Content-Type": "text/plain"},
-		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-		Body:              "Hello, World!",
-		IsBase64Encoded:   false,
+		StatusCode: 200,
+		Headers:    map[string]string{"Content-Type": "text/plain"}, //application/json
+		Body:       "Hello, World!",
 	}, nil
 }
 
