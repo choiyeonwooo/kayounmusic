0216a770b5bfafe2180ca15a56f4511b831e133f
Brian Min <bymi15@yahoo.com>
2022-12-12 23:09:54 +0000

    update desc

--- src/components/about/BiographySection.js
+++ src/components/about/BiographySection.js
@@ -9,17 +9,22 @@ function BiographySection() {
           <div className="biography col-md-6">
             <h2 className="pb-3">Biography</h2>
             <p>
-              Ka Youn Yoo <small>(2000-present)</small> is a composer from South Korea, Ka Youn’s
-              ambition is to express the sound of international music and arts through her
-              compositions. In October 2022, her composition ‘Han’ which she wrote about different
-              sorrowness that Korean women in Japanese colonial area have had, has been performed as
-              the winning piece of the LaonG Gayageum (Korean traditional zither) composition
-              competition.
+              Ka Youn Yoo <small>(2000-present)</small> is a composer from South Korea, currently
+              studying for a masters degree in Composition for Screen at Royal College of Music,
+              having graduated from King’s College London and The Purcell School for Young Musicians
+              in the UK. Her works have been premiered in various venues across the world, including
+              Cadogan Hall in London, Korean Ministry of Unification and National Gugak Center in
+              South Korea, Dobříš Castle in Czech Republic, and the Government House in Antigua and
+              Barbuda.
             </p>
             <p>
-              In 2019, she won the Purcell School’s annual orchestral composition commission for
-              which she wrote a unique orchestral fusion ‘Taepyeong’(Peace), which was performed by
-              the Purcell School Symphony Orchestra in their annual flagship concert at the Cadogan
+              Ka Youn’s ambition is to express the sound of international music and arts through her
+              compositions. In October 2022, her composition ‘Han’ which she wrote about the sorrow
+              of Korean women in Japanese colonial period, has been performed as the winning piece
+              of the LaonG Gayageum (Korean traditional zither) composition competition. In 2019,
+              she won the Purcell School’s annual orchestral composition commission for which she
+              wrote a unique orchestral fusion ‘Taepyeong’(Peace), which was performed by the
+              Purcell School Symphony Orchestra in their annual flagship concert at the Cadogan
               Hall. The work was inspired by Korean Taepyeong-song, incorporating traditional Korean
               music, as well as elements of the Western Classical traditions, in order to create a
               kaleidoscope of different musical cultures.
@@ -30,8 +35,7 @@ function BiographySection() {
               in film where music acts as a strong medium to deliver emotions to the audience. This
               experience led her to participate in an online summer program in Film Scoring Academy
               of Europe in July 2020. After that experience, she began her journey as a film
-              composer and is now working on several short film projects including Marked
-              <small>(2021)</small> and Outsider <small>(2021)</small> with young film directors
+              composer and is now working on several short film projects with young film directors
               across London.
             </p>
             <p>
