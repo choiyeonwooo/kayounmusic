b27df7769569c39ed05836c4421de8dc3f351ab9
Brian Min <bymi15@yahoo.com>
2022-12-12 23:10:52 +0000

    update youtube url

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,7 @@ const config = {
   API_ENDPOINT: "https://kayoun.com/api",
   FACEBOOK_URL: "https://www.facebook.com/Ka-Youn-Yoo-Composer-103491528169024",
   INSTAGRAM_URL: "https://www.instagram.com/kayoun_yoo",
-  YOUTUBE_URL: "https://www.youtube.com/channel/UCj3OZleY0PqZUU6hoPcv9bA",
+  YOUTUBE_URL: "https://www.youtube.com/@kayoun_yoo",
   SOUNDCLOUD_URL: "https://soundcloud.com/ka-youn-yoo",
   CONTACT_FORM_URL: "https://formspree.io/f/xayazdjd",
   EMAIL: "kbshana3338@gmail.com",
